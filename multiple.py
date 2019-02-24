396c8904c5f0ceccae9a79ca3d6ab65611feaf7a
QuentO <qochieng@gmail.com>
2019-02-24 12:42:32 -0800

    Update multiple.py
    Re-formatted functions so game core functionality is together.

--- multiple.py
+++ multiple.py
@@ -1,29 +1,15 @@
 def is_multiple(n, m):
-    "Functions takes 2 integers, checks if first is a multiple of second"
+    "Function takes 2 integers, checks if first is a multiple of second"
     
     # Use modulo to check if there is a remainder (i.e. if it's a multiple)    
     if n % m == 0:
         return True
     else:
         return False
-
-def game_header():
-    """ Display game msg or error """
-    print("*****************************************************************")
-    print("********************** Is a Multiple Of *************************")
-    print("*****************************************************************")
-
-def game_instructions():
-    """ Display the game instructions """
-    print("INSTRUCTIONS")
-    print("Get two integers from user, check if 1st is a multiple of the 2nd.")
-
-def error_display(error_msg):
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print("+            ", error_msg, "          +")
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")    
+    
 
 def play_game():
+    """ Play the 'is multiple' game """
     status = "incomplete"
     
     while status == "incomplete":
@@ -47,6 +33,26 @@ def play_game():
             # If non-numeric value is typed, show error message
             error_display("ERROR: Please type in numeric input(s).")
 
+
+# Utility Functions to help with the display in the game.
+def game_header():
+    """ Display game msg or error """
+    print("*****************************************************************")
+    print("********************** Is a Multiple Of *************************")
+    print("*****************************************************************")
+
+def game_instructions():
+    """ Display the game instructions """
+    print("INSTRUCTIONS")
+    print("Get two integers from user, check if 1st is a multiple of the 2nd.")
+
+def error_display(error_msg):
+    """ Display error messages """
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("+            ", error_msg, "          +")
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+
 def main():
     "Get two integers from user, check if 1st is multiple of 2nd"
     
@@ -54,7 +60,7 @@ def main():
     game_header()
     # Show the game instructions
     game_instructions()
-
+    # Play the game
     play_game()
             
     
